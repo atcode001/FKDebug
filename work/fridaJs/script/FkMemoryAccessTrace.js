c4a9cf7a9342ec6d92c1923c45a4d2c98a935a07
pb5315 <531567990@qq.com>
2026-08-21 11:54:29 +0800

    refactor: 将 fridaJs 替换为子模块，指向 FKDebugJS 仓库

--- work/fridaJs/script/FkMemoryAccessTrace.js
+++
@@ -1,9 +0,0 @@
-﻿jsname = "FkMemoryAccessTrace";
-
-
-
-var mbase = Module.getBaseAddress('libsgmainso-6.5.115.so')
-var mbpt = new MemoryBreakPointTrace((data) => {
-    fKLog.kCLog(data);
-});
-mbpt.Add(mbase.add(0x3C884),4,"int3")
